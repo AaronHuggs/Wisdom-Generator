d6861e6a63000be4d80ab7fb0fdcf72e6423393d
Aaron Huggins <huggins.aaron@outlook.com>
2021-09-10 13:59:35 -0700

    Created three banks of word parts (8 each)

--- script.js
+++ script.js
@@ -2,9 +2,13 @@
     Wisdom Generator!
 
     To Do:
-        []Create three banks of word parts: Beginning, Middle, End.
+        [•]Create three banks of word parts: Beginning, Middle, End.
             For example: |You should| |do jumping jacks| |all over.|
         []Randomly select a word part from each bank, combine them, and return the entire string.
             For example: "The ancient ones have spoken! They say: 'You should do jumping jacks all over.'"
 
 */
+
+const Beginning = ['You should','You must','Why don\'t you','Try to','You can\'t','You shouldn\'t','Don\'t ever','Please don\'t'];
+const Middle = ['buy a bicyle','fly a kite','eat too much','be better','pet a cat','love yourself','read books','open your eyes'];
+const End = ['today.','tomorrow.','every day.','forever.','for eternity.','or something.','or whatever.','or don\'t.'];
