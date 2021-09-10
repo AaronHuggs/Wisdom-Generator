e60fbe8fceaf14b36a683a7c7e570507b6016dfd
Aaron Huggins <huggins.aaron@outlook.com>
2021-09-10 13:42:20 -0700

    Defined a list of initial project tasks.

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+    Wisdom Generator!
+
+    To Do:
+        []Create three banks of word parts: Beginning, Middle, End.
+            For example: |You should| |do jumping jacks| |all over.|
+        []Randomly select a word part from each bank, combine them, and return the entire string.
+            For example: "The ancient ones have spoken! They say: 'You should do jumping jacks all over.'"
+
+*/
