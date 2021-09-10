1fb53590ec91b55fea36a883573df22b26b29949
Aaron Huggins <huggins.aaron@outlook.com>
2021-09-10 14:23:50 -0700

    Built GenerateWisdom(). Updated tasks.

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 /*
     Wisdom Generator!
+        by @AaronHuggs
 
     To Do:
         [•]Create three banks of word parts: Beginning, Middle, End.
             For example: |You should| |do jumping jacks| |all over.|
-        []Randomly select a word part from each bank, combine them, and return the entire string.
+        [•]Randomly select a word part from each bank, combine them, and return the entire string.
             For example: "The ancient ones have spoken! They say: 'You should do jumping jacks all over.'"
+        [ ]Add another bank for introductions, to randomize the beginning of the message as well.
 
 */
 
+//Define three banks of word parts to draw response from: Beginning, Middle, End.
 const Beginning = ['You should','You must','Why don\'t you','Try to','You can\'t','You shouldn\'t','Don\'t ever','Please don\'t'];
 const Middle = ['buy a bicyle','fly a kite','eat too much','be better','pet a cat','love yourself','read books','open your eyes'];
 const End = ['today.','tomorrow.','every day.','forever.','for eternity.','or something.','or whatever.','or don\'t.'];
+
+/**
+ * Generates the wisdom using the sacred magic arts.
+ * 
+ * Selects a word part from each bank and combines them to form a string.
+ * 
+ * @returns {string} The combined string to be output.
+ */
+const GenerateWisdom = () => {
+    //Generate random numbers based on the length of each array.
+    const randBeg = Math.floor(Math.random() * Beginning.length);
+    const randMid = Math.floor(Math.random() * Middle.length);
+    const randEnd = Math.floor(Math.random() * End.length);
+
+    //Assemble string using random word parts, and output.
+    const wisdom = `The ancient ones have spoken!\nThey say: ${Beginning[randBeg]} ${Middle[randMid]} ${End[randEnd]}`;
+    return wisdom;
+}
+
+//Print wisdom to console.
+console.log(GenerateWisdom());
